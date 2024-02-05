8ddca7dae67f805cef48f7766da29f3d97a8ce5e
Marcos Eduardo Winiarski <marcoswinrar@gmail.com>
2024-02-04 23:30:02 -0300

    feat: Creating navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link"
+
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="mx-auto max-w-5xl px-6">
+      <div className="flex justify-between items-center h-16 w-full">
+        <Link href={"/"}>Geeker Code</Link>
+        <div>Navbar</div>
+        <div>theme</div>
+      </div>
     </div>
   )
 }
