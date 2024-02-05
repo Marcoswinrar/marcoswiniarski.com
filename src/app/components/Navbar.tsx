30b033f93b078b7dc09c7f3bcb522c4539f29951
Marcos Eduardo Winiarski <marcoswinrar@gmail.com>
2024-02-05 00:31:15 -0300

    add: Theme switcher to navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import ThemeSwitcher from "./ThemeSwitcher"
 
 const Navbar = () => {
   return (
@@ -6,7 +7,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center h-16 w-full">
         <Link href={"/"}>Geeker Code</Link>
         <div>Navbar</div>
-        <div>theme</div>
+        <ThemeSwitcher />
       </div>
     </div>
   )
