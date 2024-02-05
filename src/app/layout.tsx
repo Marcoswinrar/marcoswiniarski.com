75db654c2b970d6fa59f9cbff926106db188a981
Marcos Eduardo Winiarski <marcoswinrar@gmail.com>
2024-02-05 00:31:50 -0300

    add: new font and style in dark/mode classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Fira_Code } from "next/font/google";
 import Navbar from "./components/Navbar";
+import { Provider } from "./utils/Provider";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fira = Fira_Code({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Geeker Code Blog",
@@ -17,9 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
-        {children}
+      <body className={`${fira.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}>
+        <Provider>
+          <Navbar />
+          <main className="mx-auto max-w-5xl px-6">
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
